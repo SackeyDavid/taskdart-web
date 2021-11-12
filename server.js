6bda70896e3eb2b0d03633af994839229be2e94c
David <kofiahendev@gmail.com>
2021-11-12 14:56:07 +0000

    changed name in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/kinedu'));
+app.use(express.static('./dist/taskdart-web'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/kinedu/'}),
+    res.sendFile('index.html', {root: 'dist/taskdart-web/'}),
 );
 
 // Start the app by listening on the default Heroku port
